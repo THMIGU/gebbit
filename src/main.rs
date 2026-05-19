746876c759fd31f0844fb5e9f3236a10a37e4caa
Kai Parsons <thmigu.kp@gmail.com>
2026-05-18 18:28:11 -0700

    feat: window loading from sdl3-test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use sdl3::{event::Event, pixels::Color};
+use std::time::{Duration, Instant};
+
+const TICK_RATE: f64 = 120_f64;
+
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+
 fn main() {
-    println!("Hello, world!");
+	let sdl_context = sdl3::init().unwrap();
+	let video_subsystem = sdl_context
+		.video()
+		.unwrap();
+
+	let window = video_subsystem
+		.window("gebbit", WINDOW_WIDTH, WINDOW_HEIGHT)
+		.position_centered()
+		.build()
+		.unwrap();
+
+	let mut canvas = window.into_canvas();
+
+	let mut event_pump = sdl_context
+		.event_pump()
+		.unwrap();
+
+	let mut last_frame = Instant::now();
+	let mut accumulator = Duration::new(0, 0);
+	let tick_time = Duration::from_secs_f64(1_f64 / TICK_RATE);
+
+	'running: loop {
+		let now = Instant::now();
+		accumulator += now.duration_since(last_frame);
+		last_frame = now;
+
+		for event in event_pump.poll_iter() {
+			match event {
+				Event::Quit {
+					..
+				} => break 'running,
+				_ => {}
+			}
+		}
+
+		while accumulator >= tick_time {
+			accumulator -= tick_time;
+		}
+
+		canvas.set_draw_color(Color::BLACK);
+		canvas.clear();
+
+		canvas.present();
+	}
 }
